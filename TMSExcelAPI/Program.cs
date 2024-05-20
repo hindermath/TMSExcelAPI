9ecca1c018fbd937ec24cd34064a6b8d1dcd70ff
Thorsten Hindermann <hindermath@googlemail.com>
2024-05-20 23:16:45 +0200

    Update formula in cell 2,3
    Modified the formula in cell (2,3) from "=Sum(A2:A3)" to "=Sum(A2:B2)". This change alters the range of cells whose values are being summed, ensuring the correct calculations.

--- TMSExcelAPI/Program.cs
+++ TMSExcelAPI/Program.cs
@@ -8,7 +8,7 @@ xls.SetCellValue(1,1, "Hello World");
 
 xls.SetCellValue(2,1, 3);
 xls.SetCellValue(2,2, 4);
-xls.SetCellValue(2,3, new TFormula("=Sum(A2:A3)"));
+xls.SetCellValue(2,3, new TFormula("=Sum(A2:B2)"));
 
 xls.Save(System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "output.xlsx"));
 
