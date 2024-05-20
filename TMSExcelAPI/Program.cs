e451c1d7402cd16699b94c555ed9b9245576ad8d
Thorsten Hindermann <hindermath@googlemail.com>
2024-05-20 23:13:41 +0200

    Implement FlexCel in TMSExcelAPI
    This commit introduces the use of the FlexCel library to the TMSExcelAPI. It replaces the basic 'Hello World' console output with Excel spreadsheet generation using FlexCel. Now, the program will create an Excel file, add some values, formulas and then save it. The FlexCel package has also been added to the project dependencies.

--- TMSExcelAPI/Program.cs
+++ TMSExcelAPI/Program.cs
@@ -1,3 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using FlexCel.Core;
+using FlexCel.XlsAdapter;
 
-Console.WriteLine("Hello, World!");
+XlsFile xls = new XlsFile(1, TExcelFileFormat.v2023, true);
+
+xls.SetCellValue(1,1, "Hello World");
+
+xls.SetCellValue(2,1, 3);
+xls.SetCellValue(2,2, 4);
+xls.SetCellValue(2,3, new TFormula("=Sum(A2:A3)"));
+
+xls.Save(System.IO.Path.Combine(System.IO.Directory.GetCurrentDirectory(), "output.xlsx"));
+
